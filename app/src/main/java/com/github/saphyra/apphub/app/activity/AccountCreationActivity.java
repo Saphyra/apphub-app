c3ce0a5ed3874dae6c348c9141502ff9118ec9b8
bondabear@gmail.com <Saphy371321>
2020-09-13 12:31:02 +0200

    Add buttons to Register activity

--- app/src/main/java/com/github/saphyra/apphub/app/activity/AccountCreationActivity.java
+++ app/src/main/java/com/github/saphyra/apphub/app/activity/AccountCreationActivity.java
@@ -1,6 +1,15 @@
 package com.github.saphyra.apphub.app.activity;
 
-import android.app.Activity;
+import android.os.Bundle;
 
-public class AccountCreationActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.saphyra.apphub.app.R;
+
+public class AccountCreationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account_creation);
+    }
 }
