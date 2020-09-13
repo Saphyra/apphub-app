75268c8cb48ecc8427f70c68abfabbbe586a6761
bondabear@gmail.com <Saphy371321>
2020-09-13 13:31:24 +0200

    Store inputs at main screen

--- app/src/main/java/com/github/saphyra/apphub/app/activity/MainActivity.java
+++ app/src/main/java/com/github/saphyra/apphub/app/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.github.saphyra.apphub.app.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((EditText) findViewById(R.id.serverAddress))
+            .setText(getPreferences(Context.MODE_PRIVATE).getString(getString(R.string.server_address_key), ""));
+
+        ((EditText) findViewById(R.id.loginEmail))
+            .setText(getPreferences(Context.MODE_PRIVATE).getString(getString(R.string.remember_my_email_key), ""));
+
+        ((CheckBox) findViewById(R.id.offlineModeInput))
+            .setChecked(getPreferences(Context.MODE_PRIVATE).getBoolean(getString(R.string.offline_mode_key), false));
+
+        ((CheckBox) findViewById(R.id.rememberMyEmail))
+            .setChecked(getPreferences(Context.MODE_PRIVATE).getBoolean(getString(R.string.remember_my_email_key), false));
     }
 
     public void navigateToAccountCreation(View view) {
@@ -40,24 +54,38 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
+        final boolean rememberMyEmail = ((CheckBox) findViewById(R.id.rememberMyEmail)).isChecked();
+        final String email = ((EditText) findViewById(R.id.loginEmail)).getText().toString();
+
         final MainActivity activity = this;
         WebLayer.getRequest(
-                activity,
-                Endpoints.CHECK_SERVER_AVAILABILITY,
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        Log.i(TAG, "navigateToAccountCreation: Server is accessible.");
-                        startActivity(intent);
-                    }
-                },
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        Log.i(TAG, "navigateToAccountCreation: Server cannot be accessed.");
-                        Toast.makeText(activity, "Server cannot be accessed", Toast.LENGTH_LONG).show();
-                    }
+            activity,
+            Endpoints.CHECK_SERVER_AVAILABILITY,
+            new Runnable() {
+                @Override
+                public void run() {
+                    Log.i(TAG, "navigateToAccountCreation: Server is accessible.");
+                    saveInputs(rememberMyEmail, email);
+                    startActivity(intent);
                 }
+            },
+            new Runnable() {
+                @Override
+                public void run() {
+                    Log.i(TAG, "navigateToAccountCreation: Server cannot be accessed.");
+                    Toast.makeText(activity, "Server cannot be accessed", Toast.LENGTH_LONG).show();
+                }
+            }
         );
+    }
+
+    private void saveInputs(boolean rememberMyEmail, String email) {
+        SharedPreferences.Editor editor = getPreferences(Context.MODE_PRIVATE)
+            .edit();
+        editor.putString(getString(R.string.server_address_key), BasicConfig.SERVER_ADDRESS);
+        editor.putBoolean(getString(R.string.offline_mode_key), BasicConfig.OFFLINE_MODE);
+        editor.putString(getString(R.string.login_email_key), rememberMyEmail ? email : "");
+        editor.putBoolean(getString(R.string.remember_my_email_key), rememberMyEmail);
+        editor.apply();
     }
 }
